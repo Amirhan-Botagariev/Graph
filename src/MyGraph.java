b1fe295abce36ba59ea7b50752a9682851d4a028
Amirhan-Botagariev <amirkhan1botagariev@mail.ru>
2023-05-31 22:32:53 +0600

    Added 2 methods: addVertex and addEdge

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -7,5 +7,14 @@ public class MyGraph<V> {
         adjacencyList = new HashMap<>();
     }
 
+    public void addVertex(Vertex<V> vertex) {
+        adjacencyList.put(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(Vertex<V> source, Vertex<V> destination, double weight) {
+        Edge<V> edge = new Edge<>(source, destination, weight);
+        adjacencyList.get(source).add(edge);
+    }
+
 
 }
