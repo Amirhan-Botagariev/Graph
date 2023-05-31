fe9d4719a608fd4bb60664f4588aefd041ef4d31
Amirhan-Botagariev <amirkhan1botagariev@mail.ru>
2023-05-31 22:31:02 +0600

    redid Vertex class, added new field previous, constructor, added new getters and setters

--- src/Vertex.java
+++ src/Vertex.java
@@ -3,27 +3,46 @@ import java.util.Map;
 public class Vertex<V> {
     private V data;
     private Map<Vertex<V>, Double> adjVertex;
+    private double distance;
+    private Vertex<V> previous;
 
-    public void addAdjVertex(Vertex<V> destination, double weight){
-        addAdjVertex(destination, weight);
+    public Vertex(V data) {
+        this.data = data;
+        this.adjVertex = new HashMap<>();
+        this.distance = Double.POSITIVE_INFINITY;
+        this.previous = null;
     }
 
-    public Map<Vertex<V>, Double> getAdjVertex(){
+    public void addAdjVertex(Vertex<V> destination, double weight) {
+        adjVertex.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjVertex() {
         return adjVertex;
     }
 
-    public V getData(){
+    public V getData() {
         return data;
     }
 
-    public Vertex(V data){
-        this.data = data;
-        this.adjVertex = new HashMap<>();
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public Vertex<V> getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Vertex<V> previous) {
+        this.previous = previous;
     }
 
     @Override
     public String toString() {
-        return "Vertex " +
-                this.data;
+        return "Vertex " + data;
     }
 }
