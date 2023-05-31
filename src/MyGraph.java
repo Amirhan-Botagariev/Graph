9021c2e47255718174cc50db453e4eca643632cc
Amirhan-Botagariev <amirkhan1botagariev@mail.ru>
2023-05-31 22:32:06 +0600

    Created new MyGraph class, added constructor and a field adjencyList

---
+++ src/MyGraph.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class MyGraph<V> {
+    private Map<Vertex<V>, List<Edge<V>>> adjacencyList;
+
+    public MyGraph() {
+        adjacencyList = new HashMap<>();
+    }
+
+
+}
